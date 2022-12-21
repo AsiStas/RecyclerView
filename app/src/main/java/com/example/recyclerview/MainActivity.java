86b60ccd999b7b1281b42deb8cdfd2a829e8ca68
Asi_stas <potekhina15092003@gmail.com>
2022-12-21 08:33:07 +0300

    RecyclerView и CardView. Часть 1

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private RecyclerView recyclerView;
+    //private RecyclerView.Adapter adapter;
+    //private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<RecyclerViewItem> recyclerViewItems = new ArrayList<>();
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.ic_baseline_sentiment_satisfied_alt_24, "Happy", "Life is fun!" ));
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.ic_baseline_sentiment_very_dissatisfied_24, "Sad", "Life is sad!" ));
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.ic_baseline_sentiment_satisfied_24, "Neutral", "Life is life!" ));
+
+    }
+}
